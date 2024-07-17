b24e53867efac773cf2e007dbc4312342f5e7c76
Kristján Oddsson <koddsson@gmail.com>
2024-07-17 08:26:02 +0200

    Remove a probably unneeded `&'static` lifetime
    Co-authored-by: Keith Cirkel <keithamus@users.noreply.github.com>

--- src/expr.rs
+++ src/expr.rs
@@ -5,7 +5,7 @@ pub trait Expression {}
 pub struct Expr {
     // I don't fully understand why this works with a `Box<>` and fails without.
     pub left: Option<Box<Expr>>,
-    pub operator: Option<&'static Token>,
+    pub operator: Option<Token>,
     pub right: Option<Box<Expr>>,
 }
 
