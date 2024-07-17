5551d0bb23b98bec28e7f9b2555234c17d7f5246
Kristján Oddsson <koddsson@gmail.com>
2024-07-17 08:48:55 +0200

    remove comment

--- src/expr.rs
+++ src/expr.rs
@@ -3,7 +3,6 @@ use crate::token::token::Token;
 pub trait Expression {}
 
 pub struct Expr {
-    // I don't fully understand why this works with a `Box<>` and fails without.
     pub left: Option<Box<Expr>>,
     pub operator: Option<Token>,
     pub right: Option<Box<Expr>>,
