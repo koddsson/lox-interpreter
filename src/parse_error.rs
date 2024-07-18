ac3ad6e2c04ac4898f1100585284f3e5ac5363d3
Kristján Oddsson <koddsson@gmail.com>
2024-07-18 13:27:31 +0200

    remove unneeded lifetime

--- src/parse_error.rs
+++ src/parse_error.rs
@@ -2,7 +2,7 @@ use std::fmt;
 
 pub struct ParseError {}
 
-impl<'a> fmt::Display for ParseError {
+impl fmt::Display for ParseError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "A parse error occured!")
     }
